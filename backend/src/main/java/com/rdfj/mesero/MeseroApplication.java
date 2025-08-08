550d60f657fe3657c06d0fc9ccb229fe53951ff8
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-08 02:40:52 +0200

    Raíz del proyecto vacía

---
+++ backend/src/main/java/com/rdfj/mesero/MeseroApplication.java
@@ -0,0 +1,13 @@
+package com.rdfj.mesero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeseroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeseroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rdfj/mesero/MeseroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdfj.mesero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeseroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
