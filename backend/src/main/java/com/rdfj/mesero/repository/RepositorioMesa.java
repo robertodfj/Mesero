5966717b41e5a319f4f9bdf2ed59e18c263bcb27
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-10 04:05:02 +0200

    Creamos el controlador de mesa

--- backend/src/main/java/com/rdfj/mesero/repository/RepositorioMesa.java
+++ backend/src/main/java/com/rdfj/mesero/repository/RepositorioMesa.java
@@ -9,4 +9,5 @@ import com.rdfj.mesero.entity.Mesa;
 public interface RepositorioMesa extends JpaRepository<Mesa, Integer> {
     Optional<Mesa> findByNumeroMesa(Integer numeroMesa);
     boolean existsByNumeroMesa(Integer numeroMesa);
+    void deleteByNumeroMesa(Integer numeroMesa);
 } 
