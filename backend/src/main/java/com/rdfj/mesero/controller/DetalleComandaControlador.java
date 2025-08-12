cc654c26b3994b8fb8fd746c0d74da66265159f3
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-12 18:09:00 +0200

    Creamos el controlador de detalle

--- backend/src/main/java/com/rdfj/mesero/controller/DetalleComandaControlador.java
+++ backend/src/main/java/com/rdfj/mesero/controller/DetalleComandaControlador.java
@@ -1,5 +1,61 @@
 package com.rdfj.mesero.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rdfj.mesero.dto.DetalleComandaDTO;
+import com.rdfj.mesero.entity.DetalleComanda;
+import com.rdfj.mesero.mapper.DetalleComandaMapper;
+import com.rdfj.mesero.repository.RepositorioDetalleComanda;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/detalles")
 public class DetalleComandaControlador {
     
+    @Autowired
+    private RepositorioDetalleComanda repositorioDetalle;
+
+    // Añadir detalle
+    @PostMapping("/crear")
+    public ResponseEntity<?> crear(@Valid @RequestBody DetalleComandaDTO detalleComandaDTO){
+        try {
+            DetalleComanda detalleComanda = DetalleComandaMapper.dtoToDetalleComanda(detalleComandaDTO);
+            DetalleComanda nuevoDetalleComanda = repositorioDetalle.save(detalleComanda);
+
+            DetalleComandaDTO nuevoDetalleComandaDTO = DetalleComandaMapper.detalleComandaToDTO(nuevoDetalleComanda);
+
+            return ResponseEntity.ok(nuevoDetalleComandaDTO);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error al crear detalle: " +e.getMessage());
+        }
+    }
+
+    // Eliminar detalle 
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable int id){
+        try {
+            Optional<DetalleComanda> detalleComanda = repositorioDetalle.findById(id);
+            if (detalleComanda.isEmpty()) {
+                return ResponseEntity.status(404).body("El detalle no existe");
+            }
+            repositorioDetalle.delete(detalleComanda.get());
+            return ResponseEntity.ok("Detalle eliminado correctamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error al borrar detalle: " +e.getMessage());
+        }
+    }
+    // Mostrar todos
+
+    // Mostrar por id
 }
