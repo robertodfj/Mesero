391f8ce7742b97147cf95599306f476c065d9bb3
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-13 21:19:00 +0200

    Creamos la clase Usuario

---
+++ backend/src/main/java/com/rdfj/mesero/entity/Usuario.java
@@ -0,0 +1,97 @@
+package com.rdfj.mesero.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @NotBlank(message = "El nombre no puede estar vacio")
+    private String nombre;
+
+    @NotBlank(message = "El email no puede estar vacio")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacia")
+    private String password;
+
+    
+    public enum Rol{
+        Camarero,
+        Cocina,
+        Encargado
+    };
+
+    @NotNull(message = "El rol no puede estar vacio")
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    //private Bar bar;
+        
+    // Constructor
+    public Usuario(){
+
+    }
+
+    public Usuario(int id, String nombre, String email, String password, Rol rol){
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.rol = rol;
+    }
+    
+    // Getters y setters
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+}
