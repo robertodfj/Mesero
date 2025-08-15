1d941a5a25662b24bf7ff8b51d7d3a91b143845e
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-15 16:57:50 +0200

    Creamos el repositorio de usuario

---
+++ backend/src/main/java/com/rdfj/mesero/repository/RepositorioUsuario.java
@@ -0,0 +1,13 @@
+package com.rdfj.mesero.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rdfj.mesero.entity.Usuario;
+
+public interface RepositorioUsuario extends JpaRepository<Usuario, Integer> {
+    boolean existsByEmail(String email);
+    Optional<Usuario> findByNombre(String nombre);
+    Optional<Usuario> findByEmail(String email);
+}
