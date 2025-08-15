de7dfa367c525962db33296f9f55c26e9f1fc3e0
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-15 17:32:17 +0200

    Creamos el repositorio de bar

---
+++ backend/src/main/java/com/rdfj/mesero/repository/RepositorioBar.java
@@ -0,0 +1,13 @@
+package com.rdfj.mesero.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rdfj.mesero.entity.Bar;
+
+public interface RepositorioBar extends JpaRepository<Bar, Integer>{
+    Optional<Bar> findByNombre(String nombre);
+    Optional<Bar> findById(Integer id);
+    boolean existsByEmail(String email);
+} 
