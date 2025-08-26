52ea7ef9b1d66dc40fa5ed2f9063536f45fd88dc
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-26 12:15:56 +0200

    Creamos el DTO de bar

---
+++ backend/src/main/java/com/rdfj/mesero/dto/BarDTO.java
@@ -0,0 +1,83 @@
+package com.rdfj.mesero.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rdfj.mesero.entity.Mesa;
+import com.rdfj.mesero.entity.Usuario;
+
+public class BarDTO {
+    private int id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private List<Mesa> mesas = new ArrayList<>();
+    private List<Usuario> usuarios = new ArrayList<>();
+
+     // Constructor
+
+    public BarDTO(){
+
+    }
+
+    public BarDTO(int id, String nombre, String direccion, 
+                String telefono, List<Mesa> mesas, List<Usuario> usuarios){
+
+                    this.id = id;
+                    this.nombre = nombre;
+                    this.direccion = direccion;
+                    this.telefono = telefono;
+                    this.mesas = mesas;
+                    this.usuarios = usuarios;
+
+                }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Mesa> getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(List<Mesa> mesas) {
+        this.mesas = mesas;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+}
