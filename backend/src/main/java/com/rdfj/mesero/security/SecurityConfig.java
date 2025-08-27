e2c64a7a7b64081bef5cb44bde0e9e76cee2d1ed
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-27 12:06:05 +0200

    Añadimos el passwordEncoder en la configuracion para utilizarlo en el servicio de usuario

--- backend/src/main/java/com/rdfj/mesero/security/SecurityConfig.java
+++ backend/src/main/java/com/rdfj/mesero/security/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.rdfj.mesero.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class SecurityConfig {
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
