d62e10385884d5338046ee26331d989dcfa5f443
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-27 12:26:47 +0200

    Creamos el DTO de login usuarios

---
+++ backend/src/main/java/com/rdfj/mesero/dto/LoginDTO.java
@@ -0,0 +1,30 @@
+package com.rdfj.mesero.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginDTO {
+    @NotBlank(message = "El email no puede estar vacio")
+    private String email;
+    
+    @NotBlank(message = "La contraseña no puede estar vacia")
+    private String password;
+
+    public LoginDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
