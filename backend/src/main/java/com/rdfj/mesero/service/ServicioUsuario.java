b0ef5c8711b7736ba57f4fe27a4d6e4b94abd658
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-27 12:21:42 +0200

    Editamos el servicio de usuario para registrar usuarios correctamente a traves del dto y realizarlo de manera segura

--- backend/src/main/java/com/rdfj/mesero/service/ServicioUsuario.java
+++ backend/src/main/java/com/rdfj/mesero/service/ServicioUsuario.java
@@ -1,13 +1,16 @@
 package com.rdfj.mesero.service;
 
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.rdfj.mesero.dto.RegistrarUsuarioDTO;
+import com.rdfj.mesero.entity.Bar;
 import com.rdfj.mesero.entity.Usuario;
+import com.rdfj.mesero.repository.RepositorioBar;
 import com.rdfj.mesero.repository.RepositorioUsuario;
 
 @Service
@@ -16,41 +19,53 @@ public class ServicioUsuario {
     @Autowired
     private RepositorioUsuario repositorioUsuario;
 
-    // Crear usuarios
-    public Usuario añadirUsuario(Usuario usuario){
+    @Autowired 
+    private RepositorioBar repositorioBar;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Registrar usuario (Usamos el dto)
+    public Usuario registrarUsuario(RegistrarUsuarioDTO dto){
+        Bar bar = repositorioBar.findById(dto.getBarId())
+            .orElseThrow(() -> new RuntimeException("Bar no encontrado"));
+        
+
+        if(repositorioUsuario.existsByEmail(dto.getEmail())) {
+            throw new RuntimeException("El email ya está registrado");
+        }
+        Usuario usuario = new Usuario();
+        usuario.setNombre(dto.getNombre());
+        usuario.setPassword(passwordEncoder.encode(dto.getPassword()));
+        usuario.setEmail(dto.getEmail());
+        usuario.setBar(bar);
+        usuario.setRol(dto.getRol());
+
         return repositorioUsuario.save(usuario);
     }
 
-    // Eliminar usuarios
-    public void eliminarUsuario(String nombre) {
-        Usuario usuario = repositorioUsuario.findByNombre(nombre)
+    // Eliminar usuario por email
+    public void eliminarUsuario(String email){
+        Usuario usuario = repositorioUsuario.findByEmail(email)
             .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
         repositorioUsuario.delete(usuario);
     }
 
-    // Mostrar todos 
+    // Mostrar todos
     public List<Usuario> mostrarTodos(){
         return repositorioUsuario.findAll();
     }
 
-    // Buscar por nombre
-    public Usuario mostrarUsuarioNombre(String nombre){
-         Usuario usuario = repositorioUsuario.findByNombre(nombre)
-            .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        return usuario;
-    }
-
     // Buscar por email
-    public Usuario mostrarUsuarioEmail(String email){
-        Usuario usuario = repositorioUsuario.findByEmail(email)
+    public Usuario buscarPorEmail(String email){
+        return repositorioUsuario.findByEmail(email)
             .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        return usuario;
     }
 
-    // Buscar por id
-    public Usuario mostrarUsuarioId(Integer id){
-        Usuario usuario = repositorioUsuario.findById(id)
+    // Buscar por nombre
+    public Usuario buscarPorNombre(String nombre){
+        return repositorioUsuario.findByNombre(nombre)
             .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        return usuario;
     }
 }
