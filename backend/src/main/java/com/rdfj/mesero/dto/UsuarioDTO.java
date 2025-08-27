7e8925beaceda8dff96030dfffd3ce475a4caa35
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-27 11:48:37 +0200

    Creamos el DTO de usuario

---
+++ backend/src/main/java/com/rdfj/mesero/dto/UsuarioDTO.java
@@ -0,0 +1,58 @@
+package com.rdfj.mesero.dto;
+
+import com.rdfj.mesero.entity.Usuario.Rol;
+
+public class UsuarioDTO {
+    private int id;
+    private String nombre;
+    private String email;
+    private BarDTO bar;
+    private Rol rol;
+
+    // Constructor 
+    public UsuarioDTO() {}
+
+    public UsuarioDTO(int id, String nombre, String email, BarDTO bar, Rol rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.bar = bar;
+        this.rol = rol;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BarDTO getBar() {
+        return bar;
+    }
+    public void setBar(BarDTO bar) {
+        this.bar = bar;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+}
