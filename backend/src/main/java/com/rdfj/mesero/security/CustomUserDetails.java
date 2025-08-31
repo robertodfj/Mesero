c856b5f87fbe63853b01a33506cd2ee3e76e7c40
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-08-31 17:28:20 +0200

    Creamos el custom users details para establecer el isenabled de la cuenta antes y despues del token

---
+++ backend/src/main/java/com/rdfj/mesero/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.rdfj.mesero.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.rdfj.mesero.entity.Usuario;
+
+public class CustomUserDetails implements UserDetails {
+    
+    private final Usuario usuario;
+
+    public CustomUserDetails(Usuario usuario){
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + usuario.getRol()));
+    }
+
+    // Metodos obligatorios de user details
+   @Override
+   public String getPassword(){
+        return usuario.getPassword();
+   }
+
+   @Override
+   public String getUsername(){
+        return usuario.getNombre();
+   }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
