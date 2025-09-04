4e3a39318c219a23bdda48d16f900f1d173dc273
Roberto Frutos Jimenez <robertodfj93@gmail.com>
2025-09-04 16:16:49 +0200

    Editamos un fallo en auth controler al ser declarado como @controller y no como @restcontroller

--- backend/src/main/java/com/rdfj/mesero/controller/AuthController.java
+++ backend/src/main/java/com/rdfj/mesero/controller/AuthController.java
@@ -2,16 +2,16 @@ package com.rdfj.mesero.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rdfj.mesero.dto.LoginDTO;
 import com.rdfj.mesero.dto.RegistrarUsuarioDTO;
 import com.rdfj.mesero.service.AuthService;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
 
